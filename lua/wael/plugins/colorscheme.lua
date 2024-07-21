ff43c787bba4795db9a984d6978534b3c83a2354
wael-boudissaa <jw_boudissa@esi.dz>
2024-07-21 22:06:15 +0100

    v01

--- lua/wael/plugins/colorscheme.lua
+++ lua/wael/plugins/colorscheme.lua
@@ -11,15 +11,34 @@
 -- }
 return {
 
-	"oxfist/night-owl.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
+	"olivercederborg/poimandres.nvim",
+	lazy = false,
+	priority = 1000,
 	config = function()
-		-- load the colorscheme here
-		require("night-owl").setup()
-		vim.cmd.colorscheme("night-owl")
+		require("poimandres").setup({
+			-- leave this setup function empty for default config
+			-- or refer to the configuration section
+			-- for configuration options
+		})
+	end,
+
+	-- optionally set the colorscheme within lazy config
+	init = function()
+		vim.cmd("colorscheme poimandres")
 	end,
 }
+
+-- return {
+--
+-- 	"oxfist/night-owl.nvim",
+-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+-- 	priority = 1000, -- make sure to load this before all the other start plugins
+-- 	config = function()
+-- 		-- load the colorscheme here
+-- 		require("night-owl").setup()
+-- 		vim.cmd.colorscheme("night-owl")
+-- 	end,
+-- }
 -- return {
 --   {
 --     "folke/tokyonight.nvim",
